7901f8a675abb6f1d661a655ffcca64e32de0613
fcalderan19 <132412709+fcalderan19@users.noreply.github.com>
2023-12-06 21:10:51 -0300

    update netstats

--- netstats.py
+++ netstats.py
@@ -17,70 +17,73 @@ CICLO_DE_N_ARTICULOS = "ciclo"
 
 
 def main(grafo):
-    for linea in sys.stdin:
+   for linea in sys.stdin:
         comando, parametros = leer_linea(linea)
 
-    if comando == LISTAR_OPERACIONES:
-        listado_operaciones()
+        if comando == LISTAR_OPERACIONES:
+            listado_operaciones()
 
-    elif comando == CAMINO_MAS_CORTO:
-        parametros = parametros.split(",")
-        origen = parametros[0]
-        destino = parametros[1]
+        elif comando == CAMINO_MAS_CORTO:
+            parametros = parametros.split(",")
+            origen = parametros[0]
+            destino = parametros[1]
 
-        recorrido = camino(grafo, origen, destino)
-        if recorrido == None:
-            print("No se encontro recorrido")
-        else:
+            recorrido = camino(grafo, origen, destino)
+            if recorrido == None:
+                print("No se encontro recorrido")
+            else:
+                print(" -> ".join(recorrido))
+                print("Costo: ", len(recorrido) - 1)
+
+        elif comando == DIAMETRO:
+            recorrido, diam = diametroRed(grafo)
             print(" -> ".join(recorrido))
-            print("Costo: ", len(recorrido) - 1)
+            print("Costo: ", diam)
 
-    elif comando == DIAMETRO:
-        recorrido, diam = diametroRed(grafo)
-        print(" -> ".join(recorrido))
-        print("Costo: ", diam)
+        elif comando == TODOS_EN_RANGO:
+            parametros = parametros.split(",")
+            pagina = parametros[0]
+            range = parametros[1]
+            print(rango(grafo, pagina, range))
 
-    elif comando == TODOS_EN_RANGO:
-        pagina = parametros[0]
-        range = parametros[1]
-        print(rango(grafo, pagina, range))
+        elif comando == NAVEGACION_POR_PRIMER_LINK:
+            pagina = parametros
+            print(" -> ".join(navegacion(grafo, pagina)))      
 
-    elif comando == NAVEGACION_POR_PRIMER_LINK:
-        pagina = parametros[0]
-        print(" -> ".join(navegacion(grafo, pagina)))    
+        elif comando == CONECTIVIDAD:
+            pagina = parametros[0]
+            dicc_paginas = {}
+            conectados(grafo, pagina, dicc_paginas)
 
-    elif comando == CONECTIVIDAD:
-        pagina = parametros[0]
-        dicc_paginas = {}
-        conectados(grafo, pagina, dicc_paginas)
+        elif comando == LECTURA_A_LAS_2_AM:
+            paginas = parametros
+            lectura_orden(grafo, paginas)
 
-    elif comando == LECTURA_A_LAS_2_AM:
-        paginas = parametros
-        lectura_orden(grafo, paginas)
+        elif comando == COMUNIDADES:
+            comunidad(grafo, pagina)
 
-    elif comando == COMUNIDADES:
-        comunidad(grafo, pagina)
-
-    elif comando == CLUSTERING:
-        clustering(grafo, pagina)
+        elif comando == CLUSTERING:
+            clustering(grafo, pagina)
 
 if __name__ == '__main__':
     stdin = sys.argv
     if len(stdin) != 2:
         raise Exception("Parametros invalidos")
 
-    grafo = Grafo()
+    grafo = Grafo(dirigido = True)
     archivo = sys.argv[1]
     with open(archivo, 'r') as tsv:
         for line in tsv:
-            datos = line.strip().split('\t')
+            line = line.rstrip("\n")
+            datos = line.split('\t')
             articulo = datos[0]
             links = datos[1:]
-            grafo.agregar_vertice(articulo)
+            if not grafo.verticeExistente(articulo):
+                grafo.agregar_vertice(articulo)
             for link in links:
-                if link not in grafo:
+                if not grafo.verticeExistente(link):
                     grafo.agregar_vertice(link)
-                    
-                grafo.agregar_arista(articulo, link)
-
+                if not grafo.estan_unidos(articulo, link):
+                    grafo.agregar_arista(articulo, link, 1)
+                
     main(grafo)
